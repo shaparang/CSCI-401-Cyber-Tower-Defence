eaa78d77cb9ed73f7697d325823c9e10fb728b83
elo77elo <elo77elo@aol.com>
2023-03-23 00:01:47 -0400

    Moved window into own class, fixed output

--- main.py
+++ main.py
@@ -1,42 +1,55 @@
 import tkinter as tk
 
-# Window Setup
-window = tk.Tk()
-height, width = window.winfo_screenheight(), window.winfo_screenwidth()
-winSize = str(int(width / 2)) + "x" + str(int(height / 2))  # This is a monstrosity
-window.geometry(winSize)
+class Window:
+    global display
 
-# display to hold the graphics
-display = tk.Frame(window, bg="blue", height=200, relief="sunken", bd=5)
+    # Window Setup
+    window = tk.Tk()
+    height, width = window.winfo_screenheight(), window.winfo_screenwidth()
+    winSize = str(int(width / 2)) + "x" + str(int(height / 2))  # This is a monstrosity
+    window.geometry(winSize)
 
-# user input ui
-inputVar = tk.StringVar()
-inputText = tk.Entry(window, bg="black", fg="green", bd=5, relief="sunken", font="Ubuntu", textvariable=inputVar)
+    # display to hold the graphics
+    display = tk.Frame(window, bg="blue", height=200, relief="sunken", bd=5)
 
-# text output ui  TODO a text widget can be edited unless we keep changing the state, need to find a better option
-consoleText = tk.Text(window, bg="black", fg="green", height=10, bd=5, relief="sunken", font="Ubuntu",)
+    # user input ui
+    inputVar = tk.StringVar()
+    inputText = tk.Entry(window, bg="black", fg="green", bd=5, relief="sunken", font="Ubuntu", textvariable=inputVar)
 
-# layout
-display.pack(side="top", fill="both", expand=True)
-inputText.pack(side="bottom", fill="x")
-consoleText.pack(side="bottom", fill="x")
+    # text output ui
+    consoleText = tk.Text(window, bg="black", fg="green", height=10, bd=5, relief="sunken", font="Ubuntu",
+                          state="disabled")
 
-#keyboard
-window.bind("<Return>", lambda x: inputRead())
+    # layout
+    display.pack(side="top", fill="both", expand=True)
+    inputText.pack(side="bottom", fill="x")
+    consoleText.pack(side="bottom", fill="x")
 
-# feature testing
-consoleText.insert(tk.END, "This is a test of the console system\n")
-consoleText.insert(tk.END, "The is a second test of the console system\n")
+    # keyboard binds
+    enter = tk.IntVar()
+    window.bind('<Return>', enter.set(1))
+    def console_clear(self, lines):
+        self.consoleText.delete(1.0, lines)
 
-def consoleWrite(input):
-    output = input + "\n"
-    consoleText.insert(tk.END, output)
+    def console_write(self, input):
+        self.consoleText.config(state="normal")
+        output = input + "\n"
+        self.consoleText.insert(tk.END, output)
+        self.consoleText.config(state="disabled")
 
-def inputRead():
-    userInput = inputVar.get()
-    consoleWrite(userInput)
-    inputText.delete(0, len(userInput))
-    return userInput
+    def input_read(self):
+        self.inputText.wait_variable(self.enter)
+        userInput = self.inputVar.get()
+        self.console_write(userInput)
+        self.inputText.delete(0, len(userInput))
+        self.enter.set(0)
+        return userInput
 
+
+main = Window()
+
+main.console_write("this is a test")
+var = main.input_read()
+main.console_write(var)
 
 tk.mainloop()
